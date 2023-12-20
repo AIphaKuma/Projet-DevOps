44399278dcf5b87478003b81f405da70a8959d52
k-guest <k.guest.fr@gmail.com>
2023-12-20 10:04:18 +0100

    Create Profile & Update Profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import './App.css';
+
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
 import SignIn from './SignIn';
 import SignUp from './SignUp';
-import './App.css';
-import { withAuthenticator } from '@aws-amplify/ui-react';
+import Profile from './Profile';
+import Update from './Update';
 
+import { withAuthenticator } from '@aws-amplify/ui-react';
 
 function App() {
     return (
@@ -12,10 +16,14 @@ function App() {
             <nav>
                 <Link to="/signin">Sign In</Link>
                 <Link to="/signup">Sign Up</Link>
+                <Link to="/profile">Profile</Link>
+                <Link to="/update">Update</Link>
             </nav>
             <Routes>
                 <Route path="/signin" element={<SignIn />} />
                 <Route path="/signup" element={<SignUp />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/update" element={<Update />} />
             </Routes>
         </Router>
     );
