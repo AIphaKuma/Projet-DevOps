2e862335e1b11c2b2ba0a61bc6cf40f27f615544
Raphael <raphaeltruong27@outlook.com>
2023-12-20 09:34:28 +0100

    Update SignIn.jsx

--- src/SignIn.jsx
+++ src/SignIn.jsx
@@ -1,8 +1,24 @@
 import './App.css'
+import { useState, useEffect } from 'react';
+import { Auth } from 'aws-amplify';
+
 
 import React from 'react'
 
 function SignIn() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const signInResponse = await Auth.signIn(email, password);
+            console.log('Réponse de la connexion:', signInResponse);
+            // Rediriger l'utilisateur après la connexion réussie
+        } catch (error) {
+            console.error('Erreur lors de la connexion:', error);
+        }
+    };
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -14,11 +30,16 @@ function SignIn() {
                   <form className="space-y-4 md:space-y-6" action="#">
                       <div>
                           <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                          <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""></input>
-                      </div>
+                          <input type="text" name="email" id="email"
+                                 className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                 placeholder="name@company.com"
+                                 onChange={(e) => setEmail(e.target.value)}  // Ajoutez cette ligne
+                                 required></input></div>
                       <div>
                           <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Mot de passe</label>
-                          <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""></input>
+                          <input type="password" name="password" id="password" placeholder="••••••••"
+                                 className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                 onChange={(e) => setPassword(e.target.value)} required></input>
                       </div>
                       <div className="flex items-center justify-between">
                           <a href="#" className="text-sm font-medium text-blue-500 hover:text-blue-700">Mot de passe oublié ?</a>
