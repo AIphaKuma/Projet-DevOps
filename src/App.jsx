9375a1e399502c0a683c826e3cebdd8304737c22
k-guest <k.guest.fr@gmail.com>
2023-12-20 15:27:25 +0100

    Update HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,24 @@ import './App.css';
 import React, { useEffect, useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Home from './Home';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
 import Profile from './Profile';
 import ConfirmSignUp from "./ConfirmAuth.jsx";
 import Update from './Update';
 import Navbar from './Navbar';
+import Footer from './Footer';
 
 function App() {
     return (
         <Router>
             <Navbar />
             <Routes>
+                <Route path="/" element={<><Home /><Footer /></>} />
                 <Route path="/signin" element={<SignIn />} />
                 <Route path="/signup" element={<SignUp />} />
-                <Route path={"/confirmsignup"} element={<ConfirmSignUp />} />
+                <Route path="/confirmsignup" element={<ConfirmSignUp />} />
                 <Route path="/profile" element={<Profile />} />
                 <Route path="/update" element={<Update />} />
             </Routes>
